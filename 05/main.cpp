20dcfbe19670f8042294e64e5392840d9b5be234
Alexandr Pimikov <pimikov@gmail.com>
2024-12-07 14:54:21 +0200

    day 6 part 1 passed

--- 05/main.cpp
+++ 05/main.cpp
@@ -14,27 +14,109 @@ int main() {
         return 1;
     }
     std::string line;
-    std::vector<std::string> map;
-    int i = 0;  
+    int part = 0;
+    std::vector<std::vector<int>> grid(100, std::vector<int>(100, 0)); // Adjacency matrix
+    std::vector<std::vector<int>> updates;
+    std::map<int, int> rules_right;
+    std::map<int, int> rules_left;
     while (std::getline(file, line)){
-        map.push_back(line);
-        line.find(" ");
-    }
-    file.close();
-    int pos[2] = {-1, -1};
-    for (int i = 0; i < map.size(); ++i){
-        for (int j = 0; j < line.size(); ++j){
-            if (line[j] == '>' || line[j] == '<' || line[j] == '^' || line[j] == 'v'){
-                std::cout << "Guard ound: " << line[j] << " at " << i << " " << j << std::endl;
-                pos[0] = i;
-                pos[1] = j;
-                break;
+        if (0 == part){
+            if (!line.empty()){
+                int page1 = std::stoi(line.substr(0, 2));
+                int page2 = std::stoi(line.substr(3, 2));
+                grid[page1][page2] = 1;
+                rules_right[page1] = page2;
+                rules_left[page2] = page1;
+                //std::cout << "Page1: " << page1 << " Page2: " << page2 << std::endl;
+            }
+            else { 
+                part = 1;
+                continue;
             }
         }
-        if (pos[0] != -1)
-            break;
+        else if (0 < part){
+            std::istringstream ss(line);
+            std::string token;
+            std::vector<int> values;
+            while (std::getline(ss, token, ',')){
+                int value = std::stoi(token);
+                values.push_back(value);
+
+            }
+            updates.push_back(values);
+        }
     }
-    
+    file.close();
+    std::vector<int> pages_list(updates.size(), 0);
+    for (int k = 0; k < updates.size(); ++k){
+        auto& update = updates[k];
+        int flag = 0;
+        for (int i = 0; i < update.size(); ++i){
+            if (flag)
+                break;
+            int page = update[i];
+            for (int j = 0; j < i; ++j){  // left side
+                int left_page = update[j];
+                if (1 == grid[page][left_page]){
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag)
+                break;
+            for (int j = i + 1; j < update.size(); ++j){  // right side
+                int right_page = update[j];
+                if (1 == grid[right_page][page]){
+                    flag = 1;
+                    break;
+                }
+            }
+            //std::cout << "Page: " << page << "map->" << rules_right[page] <<  std::endl;
+        }
+        if (!flag)
+            pages_list[k] = update[update.size() / 2];
+    }
+    std::cout << "Pages list: ";
+    for (auto& page : pages_list){
+        std::cout << page << " ";
+    }
+    int total = 0;
+    for (auto& page : pages_list){
+        total += page;
+    }
+    std::cout << "Total: " << total << std::endl;
+    std::vector<int> pages_list_wrong(updates.size(), 0);
+    for (int k = 0; k < updates.size(); ++k){
+        if (pages_list[k])
+            continue;
+        auto& update = updates[k];
+        int flag = 1;
+        while (flag){
+            flag = 1;
+            for (int i = 0; i < update.size() - 1; ++i){
+                int page_left = update[i];
+                int page_right = update[i + 1];
+                if (grid[update[i + 1]][update[i]]){
+                     std::swap(update[i], update[i + 1]);
+                    flag = 2;
+                    break;
+                }
+            }
+            if (1 == flag)
+                break;
+        }
+        pages_list_wrong[k] = update[update.size() / 2];
+    }
+    std::cout << "Pages list wrong: ";
+    for (auto& page : pages_list_wrong){
+        std::cout << page << " ";
+    }
+    total = 0;
+    for (auto& page : pages_list_wrong){
+        total += page;
+    }
+    std::cout << "Total wrong: " << total << std::endl;
     return 0;
 }
+//6267
 //6267
--- 06/main.cpp
+++ 06/main.cpp
@@ -8,114 +8,107 @@
 #include <map>
 
 int main() {
-    std::ifstream file("input05.txt");
+    //std::ifstream file("input06test.txt");
+    std::ifstream file("input06.txt");
     if (!file.is_open()){
         std::cerr << "Failed to  open file" << std::endl;
         return 1;
     }
     std::string line;
-    int part = 0;
-    std::vector<std::vector<int>> grid(100, std::vector<int>(100, 0)); // Adjacency matrix
-    std::vector<std::vector<int>> updates;
-    std::map<int, int> rules_right;
-    std::map<int, int> rules_left;
+    std::vector<std::string> map;
+    int i = 0;  
     while (std::getline(file, line)){
-        if (0 == part){
-            if (!line.empty()){
-                int page1 = std::stoi(line.substr(0, 2));
-                int page2 = std::stoi(line.substr(3, 2));
-                grid[page1][page2] = 1;
-                rules_right[page1] = page2;
-                rules_left[page2] = page1;
-                //std::cout << "Page1: " << page1 << " Page2: " << page2 << std::endl;
-            }
-            else { 
-                part = 1;
-                continue;
-            }
-        }
-        else if (0 < part){
-            std::istringstream ss(line);
-            std::string token;
-            std::vector<int> values;
-            while (std::getline(ss, token, ',')){
-                int value = std::stoi(token);
-                values.push_back(value);
-
-            }
-            updates.push_back(values);
-        }
+        map.push_back(line);
     }
     file.close();
-    std::vector<int> pages_list(updates.size(), 0);
-    for (int k = 0; k < updates.size(); ++k){
-        auto& update = updates[k];
-        int flag = 0;
-        for (int i = 0; i < update.size(); ++i){
-            if (flag)
+    int pos[2] = {-1, -1};
+    for (int i = 0; i < map.size(); ++i){
+        line = map[i];
+        for (int j = 0; j < line.size(); ++j){
+            if (line[j] == '>' || line[j] == '<' || line[j] == '^' || line[j] == 'v'){
+                std::cout << "Guard found: " << line[j] << " at " << i << " " << j << std::endl;
+                pos[0] = i;
+                pos[1] = j;
                 break;
-            int page = update[i];
-            for (int j = 0; j < i; ++j){  // left side
-                int left_page = update[j];
-                if (1 == grid[page][left_page]){
-                    flag = 1;
-                    break;
-                }
             }
-            if (flag)
-                break;
-            for (int j = i + 1; j < update.size(); ++j){  // right side
-                int right_page = update[j];
-                if (1 == grid[right_page][page]){
-                    flag = 1;
-                    break;
-                }
-            }
-            //std::cout << "Page: " << page << "map->" << rules_right[page] <<  std::endl;
         }
-        if (!flag)
-            pages_list[k] = update[update.size() / 2];
+        if (pos[0] != -1)
+            break;
     }
-    std::cout << "Pages list: ";
-    for (auto& page : pages_list){
-        std::cout << page << " ";
-    }
+    std::cout << "Position: " << pos[0] << " " << pos[1] << std::endl;
+    std::vector<std::vector<int>> route(map.size(), std::vector<int>(map[0].size(), 0));
+    //while (pos[0] >= 0 && pos[1] >= 0 && pos[0] < map.size() && pos[1] < map[pos[0]].size()){
     int total = 0;
-    for (auto& page : pages_list){
-        total += page;
+    while (true){
+        if (pos[0] == 0 && map[pos[0]][pos[1]] == '^')
+            break;
+        else if (pos[1] == 0 && map[pos[0]][pos[1]] == '<')
+            break;
+        else if (pos[0] == map.size() - 1 && map[pos[0]][pos[1]] == 'v')
+            break;
+        else if (pos[1] == map[pos[0]].size() - 1 && map[pos[0]][pos[1]] == '>')
+            break;
+        else if (pos[0] == map.size() - 1 && map[pos[0]][pos[1]] == 'v')
+            break;
+        else if (pos[1] == 0 && map[pos[0]][pos[1]] == '<')
+            break;
+        else if (pos[1] == map[pos[0]].size() - 1 && map[pos[0]][pos[1]] == '>')
+            break;
+        if (map[pos[0]][pos[1]] == '>'){
+            if (map[pos[0]][pos[1] + 1] == '#')
+                map[pos[0]][pos[1]] = 'v';
+            else {
+                map[pos[0]][pos[1]] = '.';
+                pos[1] += 1;
+                map[pos[0]][pos[1]] = '>';
+                total += 1;
+                route[pos[0]][pos[1]] = 1;
+            }
+        }
+        else if (map[pos[0]][pos[1]] == '<'){
+            if (map[pos[0]][pos[1] - 1] == '#')
+                map[pos[0]][pos[1]] = '^';
+            else {
+                map[pos[0]][pos[1]] = '.';
+                pos[1] -= 1;
+                map[pos[0]][pos[1]] = '<';
+                total += 1;
+                route[pos[0]][pos[1]] = 1;
+            }
+        }
+        else if (map[pos[0]][pos[1]] == '^'){
+            if (map[pos[0] - 1][pos[1]] == '#')
+                map[pos[0]][pos[1]] = '>';
+            else {
+                map[pos[0]][pos[1]] = '.';
+                pos[0] -= 1;
+                map[pos[0]][pos[1]] = '^';
+                total += 1;
+                route[pos[0]][pos[1]] = 1;
+            }
+        }
+        else if (map[pos[0]][pos[1]] == 'v'){
+            if (map[pos[0] + 1][pos[1]] == '#')
+                map[pos[0]][pos[1]] = '<';
+            else {
+                map[pos[0]][pos[1]] = '.';
+                pos[0] += 1;
+                map[pos[0]][pos[1]] = 'v';
+                total += 1;
+                route[pos[0]][pos[1]] = 1;
+            }
+        }
+        //std::cout << "Position: " << pos[0] << " " << pos[1] << " " << map[pos[0]][pos[1]] << std::endl;
     }
     std::cout << "Total: " << total << std::endl;
-    std::vector<int> pages_list_wrong(updates.size(), 0);
-    for (int k = 0; k < updates.size(); ++k){
-        if (pages_list[k])
-            continue;
-        auto& update = updates[k];
-        int flag = 1;
-        while (flag){
-            flag = 1;
-            for (int i = 0; i < update.size() - 1; ++i){
-                int page_left = update[i];
-                int page_right = update[i + 1];
-                if (grid[update[i + 1]][update[i]]){
-                     std::swap(update[i], update[i + 1]);
-                    flag = 2;
-                    break;
-                }
-            }
-            if (1 == flag)
-                break;
-        }
-        pages_list_wrong[k] = update[update.size() / 2];
-    }
-    std::cout << "Pages list wrong: ";
-    for (auto& page : pages_list_wrong){
-        std::cout << page << " ";
-    }
     total = 0;
-    for (auto& page : pages_list_wrong){
-        total += page;
+    for (int i = 0; i < route.size(); ++i){
+        for (int j = 0; j < route[i].size(); ++j){
+            total += route[i][j];
+        }
     }
-    std::cout << "Total wrong: " << total << std::endl;
+    std::cout << "Total after corrections: " << total << std::endl;
     return 0;
 }
-//6267
+
+// 4602
