f83d8042530d0a06543264e501afabfa4f0c2ba7
Alexandr Pimikov <pimikov@gmail.com>
2024-12-07 18:16:35 +0200

    minor

--- 07/main.cpp
+++ 07/main.cpp
@@ -4,6 +4,53 @@
 #include <string>
 #include <vector>
 
+// #include <iostream>
+
+// int main() {
+//     int value = 0x12345678;  // Example 4-byte integer
+//     int byte_position = 2;  // 0 for least significant byte, 3 for most significant byte
+
+//     unsigned char extracted_byte = (value >> (byte_position * 8)) & 0xFF;
+
+//     std::cout << "Extracted byte: 0x" << std::hex << static_cast<int>(extracted_byte) << std::endl;
+//     return 0;
+// }
+
+// #include <iostream>
+
+// int main() {
+//     int value = 0x12345678;  // Example 4-byte integer
+//     int byte_position = 2;  // 0 for LSB, 3 for MSB
+
+//     // Access the byte array representation of the integer
+//     unsigned char* bytes = reinterpret_cast<unsigned char*>(&value);
+
+//     unsigned char extracted_byte = bytes[byte_position];
+
+//     std::cout << "Extracted byte: 0x" << std::hex << static_cast<int>(extracted_byte) << std::endl;
+//     return 0;
+// }
+
+
+#include <iostream>
+
+//union IntBytes {
+//     int value;
+//     unsigned char bytes[sizeof(int)];
+// };
+
+// int main() {
+//     IntBytes data;
+//     data.value = 0x12345678;
+
+//     int byte_position = 2;  // 0 for LSB, 3 for MSB
+
+//     unsigned char extracted_byte = data.bytes[byte_position];
+
+//     std::cout << "Extracted byte: 0x" << std::hex << static_cast<int>(extracted_byte) << std::endl;
+//     return 0;
+// }
+
 
 int main() {
     std::ifstream file("input07.txt");
@@ -14,10 +61,10 @@ int main() {
     std::string line;
     int part = 0;
     std::vector<std::vector<int>> data;
-    std::vector<int> results;
+    std::vector<long> results;
     while (std::getline(file, line)){
         size_t pos_end = line.find(":");
-        int result = std::stoi(line.substr(0, pos_end));
+        long result = std::stol(line.substr(0, pos_end));
         std::cout << "Result: " << result << std::endl;
         results.push_back(result);
         std::string left_str = line.substr(pos_end + 1, line.size() - pos_end);
@@ -36,11 +83,14 @@ int main() {
         data.push_back(values);
     }
     file.close();
+    int max_size = 0;
     for (size_t i = 0; i < data.size(); i++){
         auto& values = data[i];
         int result = results[i];
         int flag = 0;
-        std::cout << values.size() << std::endl;
+        if (values.size() > max_size){
+            max_size = values.size();
+        }
         // for (size_t j = 0; j < values.size(); j++){
         // }
         // if (!flag){
@@ -48,5 +98,6 @@ int main() {
         //     break;
         // }
     }
+    std::cout << "Max size: " << max_size << std::endl;
     return 0;
 }
