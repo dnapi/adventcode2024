75e2f9afbcccdb44d8b619311a996cf2792a8fd0
Alexandr Pimikov <pimikov@gmail.com>
2024-12-08 10:28:28 +0200

    day 7 passed

--- 07/main.cpp
+++ 07/main.cpp
@@ -3,101 +3,135 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
-// #include <iostream>
+int fun(std::vector<long> values, long result){
+    if (values.size() == 2){
+        if (values[0] + values[1] == result || values[0] * values[1] == result){
+            return 1;
+        }
+        return 0;
+    }
+    long value1 = values.back();
+    values.pop_back();
+    long value2 = values.back();
+    values.pop_back();
+    long product = value1 * value2;
+    values.push_back(product);
+    if (product <= result && fun(values, result)){
+        return 1;
+    }
+    values.pop_back();
+    long sum = value1 + value2;
+    values.push_back(sum);
+    if (sum <= result && fun(values, result))
+        return 1;
+    return 0;
+}
 
-// int main() {
-//     int value = 0x12345678;  // Example 4-byte integer
-//     int byte_position = 2;  // 0 for least significant byte, 3 for most significant byte
-
-//     unsigned char extracted_byte = (value >> (byte_position * 8)) & 0xFF;
-
-//     std::cout << "Extracted byte: 0x" << std::hex << static_cast<int>(extracted_byte) << std::endl;
-//     return 0;
-// }
-
-// #include <iostream>
-
-// int main() {
-//     int value = 0x12345678;  // Example 4-byte integer
-//     int byte_position = 2;  // 0 for LSB, 3 for MSB
-
-//     // Access the byte array representation of the integer
-//     unsigned char* bytes = reinterpret_cast<unsigned char*>(&value);
-
-//     unsigned char extracted_byte = bytes[byte_position];
-
-//     std::cout << "Extracted byte: 0x" << std::hex << static_cast<int>(extracted_byte) << std::endl;
-//     return 0;
-// }
-
-
-#include <iostream>
-
-//union IntBytes {
-//     int value;
-//     unsigned char bytes[sizeof(int)];
-// };
-
-// int main() {
-//     IntBytes data;
-//     data.value = 0x12345678;
-
-//     int byte_position = 2;  // 0 for LSB, 3 for MSB
-
-//     unsigned char extracted_byte = data.bytes[byte_position];
-
-//     std::cout << "Extracted byte: 0x" << std::hex << static_cast<int>(extracted_byte) << std::endl;
-//     return 0;
-// }
-
+int fun_part2(std::vector<long> values, long result){
+    if (values.size() == 2){
+        if (values[0] + values[1] == result || 
+            values[0] * values[1] == result ||
+            std::stol(std::to_string(values[1]) + std::to_string(values[0])) == result
+            ){
+            return 1;
+        }
+        return 0;
+    }
+    long value1 = values.back();
+    values.pop_back();
+    long value2 = values.back();
+    values.pop_back();
+    long product = value1 * value2;
+    values.push_back(product);
+    if (product <= result && fun_part2(values, result)){
+        return 1;
+    }
+    values.pop_back();
+    long sum = value1 + value2;
+    values.push_back(sum);
+    if (sum <= result && fun_part2(values, result))
+        return 1;
+    values.pop_back();
+    std::string str = std::to_string(value1) + std::to_string(value2);
+    long comb = std::stol(str);
+    values.push_back(comb);
+    if (comb <= result && fun_part2(values, result))
+        return 1;
+    return 0;
+}
 
 int main() {
     std::ifstream file("input07.txt");
+    //std::ifstream file("input07test.txt");
     if (!file.is_open()){
         std::cerr << "Failed to  open file" << std::endl;
         return 1;
     }
     std::string line;
     int part = 0;
-    std::vector<std::vector<int>> data;
+    std::vector<std::vector<long>> data;
     std::vector<long> results;
+    long total = 0;
+    long total_part2 = 0;
     while (std::getline(file, line)){
         size_t pos_end = line.find(":");
         long result = std::stol(line.substr(0, pos_end));
-        std::cout << "Result: " << result << std::endl;
         results.push_back(result);
         std::string left_str = line.substr(pos_end + 1, line.size() - pos_end);
         std::istringstream ss(left_str);
-        std::vector<int> values;
-        //std::string token;
-        // while (std::getline(ss, token, ' ')){
-        //     int value = std::stoi(token);
-        //     values.push_back(value);
-        // }
+        std::vector<long> values;
         int value = 0;
         while (ss >> value){
-            //std::cout << "value=" << value << std::endl;
             values.push_back(value);
         }
+        std::reverse(values.begin(), values.end());
+        //std::cout << "Result: " << result;
+        if (fun(values, result)){
+            total += result;
+            // total_part2 += result;
+        }
+        // else if (fun_part2(values, result)){
+        //     total_part2 += result;
+        // }
+        // else{
+        //     std::cout << " Failed" << std::endl;
+        // }
+        // else{
+        //    std::cout << " Failed" << std::endl;
+        // }
+        if (fun_part2(values, result)){
+            total_part2 += result;
+        //    std::cout << " Passed" << std::endl;
+        }
+        // else{
+        //     std::cout << " Failed ";
+        //     for (auto& v : values){
+        //         std::cout << v << " ";
+        //     }
+        //     std::cout << std::endl;
+        // }
         data.push_back(values);
     }
     file.close();
+    std::cout << "Total: " << total << std::endl;
+    std::cout << "Total part 2: " << total_part2 << std::endl;
     int max_size = 0;
     for (size_t i = 0; i < data.size(); i++){
         auto& values = data[i];
-        int result = results[i];
+        long result = results[i];
         int flag = 0;
         if (values.size() > max_size){
             max_size = values.size();
         }
-        // for (size_t j = 0; j < values.size(); j++){
-        // }
-        // if (!flag){
-        //     std::cout << "Result: " << result << std::endl;
-        //     break;
-        // }
     }
     std::cout << "Max size: " << max_size << std::endl;
     return 0;
 }
+
+// 709063252057  too low
+
+// 1582598718861
+
+// part 2 165278151522644
